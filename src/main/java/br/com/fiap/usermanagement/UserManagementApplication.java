41a7eedec4d1cb9650e9ad98fcf49dbfb62a7f9c
Bruno Lima <bruno.lima.dev@outlook.com>
2024-08-03 16:20:12 -0300

    test: improve coverage

--- src/main/java/br/com/fiap/usermanagement/UserManagementApplication.java
+++ src/main/java/br/com/fiap/usermanagement/UserManagementApplication.java
@@ -3,9 +3,12 @@ package br.com.fiap.usermanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.Generated;
+
 @SpringBootApplication
 public class UserManagementApplication {
 
+	@Generated
 	public static void main(String[] args) {
 		SpringApplication.run(UserManagementApplication.class, args);
 	}
