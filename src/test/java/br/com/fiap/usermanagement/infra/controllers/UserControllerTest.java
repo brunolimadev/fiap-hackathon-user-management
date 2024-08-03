edafafa6342533e76b44302dc38691bf2cb4fee1
Bruno Lima <bruno.lima.dev@outlook.com>
2024-08-03 16:13:28 -0300

    chore: code refactor

--- src/test/java/br/com/fiap/usermanagement/infra/controllers/UserControllerTest.java
+++ src/test/java/br/com/fiap/usermanagement/infra/controllers/UserControllerTest.java
@@ -14,12 +14,11 @@ import java.util.UUID;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
@@ -32,103 +31,103 @@ import br.com.fiap.usermanagement.domain.entities.User;
 import br.com.fiap.usermanagement.domain.enums.UserRoleEnum;
 import br.com.fiap.usermanagement.infra.controllers.dtos.request.UserDto;
 import br.com.fiap.usermanagement.infra.controllers.dtos.response.GetUserByEmailDto;
-import br.com.fiap.usermanagement.infra.controllers.exceptions.ExceptionAdvice;
 import br.com.fiap.usermanagement.infra.controllers.exceptions.UserNotFoundException;
 import br.com.fiap.usermanagement.infra.enums.RoleEnum;
 import br.com.fiap.usermanagement.infra.gateways.mappers.GetUserByEmailMapper;
 import br.com.fiap.usermanagement.infra.gateways.mappers.UserDtoMapper;
 
-@AutoConfigureMockMvc
-@SpringBootTest
+@WebMvcTest(UserController.class)
 @ActiveProfiles("test")
-@Import(ExceptionAdvice.class)
+@SpringJUnitConfig
 class UserControllerTest {
 
-    @MockBean
-    private CreateUserInteractor createUserInteractor;
+        @MockBean
+        private CreateUserInteractor createUserInteractor;
 
-    @MockBean
-    private GetUserByEmailInteractor getUserByEmailInteractor;
+        @MockBean
+        private GetUserByEmailInteractor getUserByEmailInteractor;
 
-    private MockMvc mockMvc;
+        private MockMvc mockMvc;
 
-    @BeforeEach
-    public void setUp() {
-        UserController userController = new UserController(createUserInteractor, getUserByEmailInteractor);
-        mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
-    }
+        @BeforeEach
+        public void setUp() {
+                UserController userController = new UserController(createUserInteractor, getUserByEmailInteractor);
+                mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
+        }
 
-    @Test
-    void whenCreateUserReturnsSuccess_thenStatus200() throws Exception {
-        UserDto request = new UserDto(
-                "teste",
-                "test@test.com.br",
-                "Teste@123",
-                LocalDateTime.now(),
-                UserRoleEnum.ROLE_USER);
+        @Test
+        void whenCreateUserReturnsSuccess_thenStatus200() throws Exception {
+                UserDto request = new UserDto(
+                                "teste",
+                                "test@test.com.br",
+                                "Teste@123",
+                                LocalDateTime.now(),
+                                UserRoleEnum.ROLE_USER);
 
-        User user = UserDtoMapper.toDomain(request);
+                User user = UserDtoMapper.toDomain(request);
 
-        when(createUserInteractor.create(any(User.class))).thenReturn(user);
+                when(createUserInteractor.create(any(User.class))).thenReturn(user);
 
-        mockMvc.perform(post("/users")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(new ObjectMapper().registerModule(new JavaTimeModule()).writeValueAsString(request)))
-                .andExpect(status().isCreated());
+                mockMvc.perform(post("/users")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(new ObjectMapper().registerModule(new JavaTimeModule())
+                                                .writeValueAsString(request)))
+                                .andExpect(status().isCreated());
 
-        verify(createUserInteractor, times(1)).create(any(User.class));
-    }
+                verify(createUserInteractor, times(1)).create(any(User.class));
+        }
 
-    @Test
-    void whenCreateUserWithInvalidEmail_thenStatus400() throws Exception {
-        UserDto request = new UserDto(
-                "teste",
-                "test.com.br",
-                "Teste@123",
-                LocalDateTime.now(),
-                UserRoleEnum.ROLE_USER);
+        @Test
+        void whenCreateUserWithInvalidEmail_thenStatus400() throws Exception {
+                UserDto request = new UserDto(
+                                "teste",
+                                "test.com.br",
+                                "Teste@123",
+                                LocalDateTime.now(),
+                                UserRoleEnum.ROLE_USER);
 
-        User user = UserDtoMapper.toDomain(request);
+                User user = UserDtoMapper.toDomain(request);
 
-        when(createUserInteractor.create(any(User.class))).thenReturn(user);
+                when(createUserInteractor.create(any(User.class))).thenReturn(user);
 
-        mockMvc.perform(post("/users")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(new ObjectMapper().registerModule(new JavaTimeModule()).writeValueAsString(request)))
-                .andExpect(status().isBadRequest());
+                mockMvc.perform(post("/users")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(new ObjectMapper().registerModule(new JavaTimeModule())
+                                                .writeValueAsString(request)))
+                                .andExpect(status().isBadRequest());
 
-    }
+        }
 
-    @Test
-    void whenGetUserByEmailReturnsSuccess_thenStatus200() throws Exception {
-        GetUserByEmailDto response = new GetUserByEmailDto(
-                UUID.randomUUID(),
-                "teste",
-                "test@test.com.br",
-                "Teste@123",
-                true,
-                LocalDateTime.now(),
-                RoleEnum.ROLE_USER);
+        @Test
+        void whenGetUserByEmailReturnsSuccess_thenStatus200() throws Exception {
+                GetUserByEmailDto response = new GetUserByEmailDto(
+                                UUID.randomUUID(),
+                                "teste",
+                                "test@test.com.br",
+                                "Teste@123",
+                                true,
+                                LocalDateTime.now(),
+                                RoleEnum.ROLE_USER);
 
-        User user = GetUserByEmailMapper.toDomain(response);
+                User user = GetUserByEmailMapper.toDomain(response);
 
-        when(getUserByEmailInteractor.getUserByEmail(anyString())).thenReturn(user);
+                when(getUserByEmailInteractor.getUserByEmail(anyString())).thenReturn(user);
 
-        mockMvc.perform(get("/users?byEmail=teste@email.com"))
-                .andExpect(status().isOk());
+                mockMvc.perform(get("/users?byEmail=teste@email.com"))
+                                .andExpect(status().isOk());
 
-        verify(getUserByEmailInteractor, times(1)).getUserByEmail(anyString());
+                verify(getUserByEmailInteractor, times(1)).getUserByEmail(anyString());
 
-    }
+        }
 
-    @Test
-    void whenGetUserByEmailThrowsUserNotFountException_thenStatus500() throws Exception {
+        @Test
+        void whenGetUserByEmailThrowsUserNotFountException_thenStatus500() throws Exception {
 
-        when(getUserByEmailInteractor.getUserByEmail(anyString()))
-                .thenThrow(new UserNotFoundException("Usuário não encontrado"));
+                when(getUserByEmailInteractor.getUserByEmail(anyString()))
+                                .thenThrow(new UserNotFoundException("Usuário não encontrado"));
 
-        mockMvc.perform(get("/users?byEmail=teste@email.com"))
-                .andExpect(status().is5xxServerError());
-    }
+                mockMvc.perform(get("/users?byEmail=teste@email.com"))
+                                .andExpect(status().is5xxServerError());
+        }
 
 }
